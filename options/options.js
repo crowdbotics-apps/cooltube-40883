ec6e10203a1dcbf851fabf9a39830d0852aaed6d
Crowdbotics <team@crowdbotics.com>
2023-05-21 22:30:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cooltube-40883",
+  url: "https://cooltube-40883.botics.co",
+  api: "https://cooltube-40883.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
