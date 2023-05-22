2a73d0ac5c1af5a7782b5be5e599ad60f24485a8
ironman sonic <b2990321@gmail.com>
2023-05-22 10:40:42 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "cooltube-40883",
-  url: "https://cooltube-40883.botics.co",
-  api: "https://cooltube-40883.botics.co/api/v1"
-}
+  "name": "cooltube-40883",
+  "url": "https://cooltube-40883.botics.co",
+  "api": "https://cooltube-40883.botics.co/api/v1",
+  "initialRoute": "Articles"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
